a2516cac2e804a5d37b948827ad027c771a79077
gh <charan.raj187@gmail.com>
2021-04-22 00:58:10 +0530

    initial commit

--- com_project/ingestion/read_data_sources/mongo_read.py
+++ com_project/ingestion/read_data_sources/mongo_read.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
          '--packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" pyspark-shell'
     )
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../../../" + ".secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
