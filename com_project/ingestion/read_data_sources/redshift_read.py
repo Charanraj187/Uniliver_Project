3a95313be785fd49bb74e42ed644b7e2c5a89dd9
gh <charan.raj187@gmail.com>
2021-04-25 15:04:34 +0530

    initial commit

--- com_project/ingestion/read_data_sources/redshift_read.py
+++ com_project/ingestion/read_data_sources/redshift_read.py
@@ -47,4 +47,4 @@ if __name__ == '__main__':
 
     txn_df.show(5, False)
 
-# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" Uniliver_Project/com_project/ingestion/read_data_sources/redshift_read.py
+# spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4" com_project/ingestion/read_data_sources/redshift_read.py
